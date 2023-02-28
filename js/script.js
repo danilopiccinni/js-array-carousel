3242c0c59eaa955be68900a47768475d7dc69e5f
danilopiccinni <danilopiccinni@gmail.com>
2023-02-28 19:42:34 +0100

    feat: active sulle anteprime

--- js/script.js
+++ js/script.js
@@ -1,42 +1,58 @@
-const immagini = [
-    'img/01.webp', 'img/02.webp' , 'img/03.webp' , 'img/04.webp' , 'img/05.webp' ,
-    'https://fastly.picsum.photos/id/530/1000/500.jpg?hmac=VDNut0o4MMrdElr4WzXiLN-PqiI5GlB5bmVwFd_DZ6A',
-    'https://fastly.picsum.photos/id/786/1000/500.jpg?hmac=0ADJU0hgDIz9EZeav9xCIzUCN6wxVoc5WuPTEYOQZ74',
-    'https://fastly.picsum.photos/id/400/1000/500.jpg?hmac=GhiCnRYFGp-Y-Ib4jUOymPWvj8KQ9RS0XfaGFF7MdXo',
-    'https://fastly.picsum.photos/id/381/1000/500.jpg?hmac=jb81WyoEAGfWPpshlMZAbZpoBFUOZsjrsv7QolmSW4g',
-    'https://fastly.picsum.photos/id/1060/1000/500.jpg?hmac=Yrz0AMRpLLASgx-KrB5AMcCQZx3eAQaUI3aayxHvAOc',
-]
-
-console.log (immagini)
+const immagini = ['img/01.webp', 'img/02.webp' , 'img/03.webp' , 'img/04.webp' , 'img/05.webp' ]
 // costanti e variabili
 let contImgEl = document.getElementById ('cont-img')
 let frecciaSuEl = document.getElementById ('freccia-su')
 let frecciaGiuEl = document.getElementById ('freccia-giu')
+let contAnteprimeEl = document.getElementById ('cont-anteprime')
 
+for (i=0 ; i < immagini.length ; i++) {
+
+    let nuovaAnteprima = document.createElement ('img')
+    contAnteprimeEl.append (nuovaAnteprima)
+    nuovaAnteprima.style.height = 'calc(100% /' + immagini.length + ')'
+    nuovaAnteprima.style.width = 'calc(100 / 5)'
+    nuovaAnteprima.classList.add ('anteprime')
+    nuovaAnteprima.src = immagini[i]
+
+}
+
+const anteprime = document.querySelectorAll ('.anteprime')
 // immagine da visualizzare al caricamento della pagina
 let index = 0;
 contImgEl.src = immagini[index]
+anteprime[index].classList.add('active')
+
 
 console.log (immagini.length)
 
 frecciaSuEl.addEventListener ('click' , function() {
-    console.log(index)
-
-    index++;
-
-    if ((index) == immagini.length) {
-        index = 0 
-    }
-
-    contImgEl.src = immagini[index]
-}) 
-
-frecciaGiuEl.addEventListener ('click' , function(){
 
     if (index == 0) {
+        anteprime[index].classList.remove('active')
         index = immagini.length
     }
 
     index--;
+    
     contImgEl.src = immagini[index]
+    
+    anteprime[index].classList.add('active')
+    console.log(index)
+    anteprime[index + 1].classList.remove('active')
+    
+}) 
+
+frecciaGiuEl.addEventListener ('click' , function(){
+    
+    index++;
+    anteprime[index - 1].classList.remove('active')
+    
+    if (index == immagini.length) {
+        index = 0 
+    }
+    
+    contImgEl.src = immagini[index]
+    anteprime[index].classList.add('active')
+    
+    
 })
