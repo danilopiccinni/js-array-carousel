f55111ad8cba3f7988cbb485cd21191cd82c885d
danilopiccinni <danilopiccinni@gmail.com>
2023-02-28 14:46:46 +0100

    feat: bonus 1

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const immagini = [
     'https://fastly.picsum.photos/id/1060/1000/500.jpg?hmac=Yrz0AMRpLLASgx-KrB5AMcCQZx3eAQaUI3aayxHvAOc',
 ]
 
+console.log (immagini)
 // costanti e variabili
 let contImgEl = document.getElementById ('cont-img')
 let frecciaSuEl = document.getElementById ('freccia-su')
@@ -15,13 +16,26 @@ let frecciaGiuEl = document.getElementById ('freccia-giu')
 let index = 0;
 contImgEl.src = immagini[index]
 
+console.log (immagini.length)
+
 frecciaSuEl.addEventListener ('click' , function() {
+    console.log(index)
 
     index++;
+
+    if ((index) == immagini.length) {
+        index = 0 
+    }
+
     contImgEl.src = immagini[index]
 }) 
 
 frecciaGiuEl.addEventListener ('click' , function(){
+
+    if (index == 0) {
+        index = immagini.length
+    }
+
     index--;
     contImgEl.src = immagini[index]
 })
