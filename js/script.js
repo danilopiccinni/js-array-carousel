feb3c9f715047764ce6ab0c05222ddb84a8bbc33
danilopiccinni <danilopiccinni@gmail.com>
2023-02-28 14:23:32 +0100

    feat: milestone 2 e 3
    dimenticato di fare push separati

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const immagini = [
+    'https://fastly.picsum.photos/id/530/1000/500.jpg?hmac=VDNut0o4MMrdElr4WzXiLN-PqiI5GlB5bmVwFd_DZ6A',
+    'https://fastly.picsum.photos/id/786/1000/500.jpg?hmac=0ADJU0hgDIz9EZeav9xCIzUCN6wxVoc5WuPTEYOQZ74',
+    'https://fastly.picsum.photos/id/400/1000/500.jpg?hmac=GhiCnRYFGp-Y-Ib4jUOymPWvj8KQ9RS0XfaGFF7MdXo',
+    'https://fastly.picsum.photos/id/381/1000/500.jpg?hmac=jb81WyoEAGfWPpshlMZAbZpoBFUOZsjrsv7QolmSW4g',
+    'https://fastly.picsum.photos/id/1060/1000/500.jpg?hmac=Yrz0AMRpLLASgx-KrB5AMcCQZx3eAQaUI3aayxHvAOc',
+]
+
+// costanti e variabili
+let contImgEl = document.getElementById ('cont-img')
+let frecciaSuEl = document.getElementById ('freccia-su')
+let frecciaGiuEl = document.getElementById ('freccia-giu')
+
+// immagine da visualizzare al caricamento della pagina
+let index = 0;
+contImgEl.src = immagini[index]
+
+frecciaSuEl.addEventListener ('click' , function() {
+
+    index++;
+    contImgEl.src = immagini[index]
+}) 
+
+frecciaGiuEl.addEventListener ('click' , function(){
+    index--;
+    contImgEl.src = immagini[index]
+})
